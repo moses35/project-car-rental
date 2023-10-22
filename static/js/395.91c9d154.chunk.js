d509a2171538de090e46e695139e39d639aaa735
moses35 <117040713+moses35@users.noreply.github.com>
2023-10-22 19:43:14 +0000

    Deploying to gh-pages from @ moses35/project-car-rental@6a44638494a4373ec97e182258f8c6626bc85b55 🚀

---
+++ static/js/395.91c9d154.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[395],{7395:function(a,e,r){r.r(e);var s=r(1219),t=r(9434),u=r(1945),n=r(184);e.default=function(){var a=(0,t.v9)(u.O).favorites;return(0,n.jsx)(s.A,{adverts:a})}}}]);
+//# sourceMappingURL=395.91c9d154.chunk.js.map
